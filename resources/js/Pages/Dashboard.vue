fdfb307857355896e069c073e367590084471a3e
SiraphopSangkrit <Siraphop_sangkrit@kkumail.com>
2024-09-07 00:31:26 +0700

    Start new project using Laravel Jetstream

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+import Card from '@/Components/Card.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="w-fit mx-auto sm:px-6 lg:px-8">
+                <div class="w-fit bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+                    <div class="col-span-4  grid grid-cols-3 gap-4">
+                        <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
